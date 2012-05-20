4ae15cc9caa5b59dfc0617f40506a4fe579549b8
Eric Prestemon <ecp@prestemon.com>
2012-05-20 18:28:45 +0000

    switch over to using bootstrap icons. working on new chat interface, not there yet.

--- public/puzzle.js
+++ public/puzzle.js
@@ -28,13 +28,15 @@ $(document).ready(
         });
         $('[class*="collapse"]').on('shown hidden', function (event) {
             if (event.type == 'hidden') {
-                $(this).prev().children(".ui-icon").addClass("ui-icon-plusthick").removeClass("ui-icon-minusthick");
+                $(this).prev().children("i").addClass("icon-plus").removeClass("icon-minus");
             } else {
-                $(this).prev().children(".ui-icon").addClass("ui-icon-minusthick").removeClass("ui-icon-plusthick");
+                $(this).prev().children("i").addClass("icon-minus").removeClass("icon-plus");
             }
             resize_chat_box($("#chat-box"));
         });
         resize_chat_box($("#chat-box"));
+        $('.dropdown-toggle').dropdown();
+
     }
 );
 
@@ -44,7 +46,7 @@ function resize_chat_box(cb) {
     openchats = cb.children().filter('[class*="in"]').filter('[class*="collapse"]').children('.chat-text');
     var open_count = openchats.length;
     if (open_count == 0) {
-        console.warn("0 open chats, returning");
+//        console.warn("0 open chats, returning");
         return;
     }
     
@@ -61,9 +63,9 @@ function resize_chat_box(cb) {
         }
     );
     
-    console.warn([inner_current, target,outer_current,open_count].join(':'));
+//    console.warn([inner_current, target,outer_current,open_count].join(':'));
     var final_size = Math.floor((inner_current + target - outer_current - 10)/open_count);
-    console.warn( "resizing to " + final_size);
+//    console.warn( "resizing to " + final_size);
     
     openchats.each(
         function() {
@@ -80,14 +82,14 @@ last_seen.puzzle = new Array();
 
 
 function setup_chat_filler(type, puzzle_id) {
-    console.log ("setup" + type + ' ' + puzzle_id);
+//    console.log ("setup" + type + ' ' + puzzle_id);
     chat_filler(type,puzzle_id);
     setInterval(function() {chat_filler(type,puzzle_id);},5000);
 }
 
 function chat_filler (type, puzzle_id) {
     var last_seen_id = 0;
-    console.warn(type + ' ' + puzzle_id);
+//    console.warn(type + ' ' + puzzle_id);
     if (type === 'puzzle' && last_seen.puzzle[puzzle_id] > 0) {
         last_seen_id = last_seen.puzzle[puzzle_id];
     }
