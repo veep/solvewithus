c30009eca40e54c8c53b9eb74a6c4250c7b700a0
Eric Prestemon <ecp@prestemon.com>
2018-07-21 20:05:58 -0700

    Move selenium setup/teardown to parent class
    Also, bail out if no Test::Mojo::Role::Selenium

---
+++ t/lib/SeleniumTest.pm
@@ -0,0 +1,35 @@
+package SeleniumTest;
+use base qw(Test::Class);
+use Test::More;
+
+$ENV{MOJO_SELENIUM_DRIVER} ||= 'Selenium::PhantomJS';
+
+sub create_config: Test(startup) {
+    my $self = shift;
+    TestSetup::setup_config();
+    eval {
+        require Test::Mojo::Role::Selenium;
+    };
+    if ($@) {
+        plan skip_all => 'No selenium role';
+        return;
+    }
+
+    $self->{mojo_test} = Test::Mojo->with_roles("+Selenium")->new('SolveWith')->setup_or_skip_all;
+}
+
+sub clean_cookies : Test(setup) {
+    my $self = shift;
+    TestSetup::clear_cookies($self->{mojo_test});
+    TestSetup::use_https($self->{mojo_test});
+}
+
+sub clear_out_phantomjs : Test(shutdown) {
+    my $self = shift;
+    if ($ENV{MOJO_SELENIUM_DRIVER} =~ /PhantomJS/) {
+        note 'shutting down phantomjs';
+        $self->{mojo_test}->driver->shutdown_binary;
+    }
+}
+
+1;
